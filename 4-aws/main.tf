e31090c7f2166caf1e2387e181da4e255296e373
YoungHan Jo <verin4494@gmail.com>
2023-06-06 14:22:42 +0900

    main.tf

--- 4-aws/main.tf
+++ 4-aws/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ap-northease-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-07c2a88388bb80eb0"
   instance_type = "t2.micro"
 
   tags = {
